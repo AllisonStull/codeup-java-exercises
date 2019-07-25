b07fac7c40ff7230d7ff8180dd92f969dfdc4a41
Allison Stull <allisonstull3@gmail.com>
2019-07-25 15:54:24 -0500

    Control Statements and Loops exercise 1 part a

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
+//#2 FIZZBUZZ
 
+//public class ControlFlowExercises {
+//    static void fizzBUzz(int num) {
+//        for (int i = 1; i <= num; i++) {
+//            if (i%15==0) {
+//                System.out.println("fizzbuzz");
+//            }else if (i%5==0) {
+//                System.out.println("buzz");
+//            } else if (i%3==0) {
+//                System.out.println("fizz");
+//            } else {
+//                System.out.println(i);
+//        }
+//    }
+//}
+//    public static void main(String[] args){
+//        fizzBUzz(100);
+//    }
+//    }
 
+//#1 part a
 public class ControlFlowExercises {
-    static void fizzBUzz(int num) {
-        for (int i = 1; i <= num; i++) {
-            if (i%15==0) {
-                System.out.println("fizzbuzz");
-            }else if (i%5==0) {
-                System.out.println("buzz");
-            } else if (i%3==0) {
-                System.out.println("fizz");
-            } else {
-                System.out.println(i);
+    public static void main(String[] args) {
+        System.out.println();
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i);
+            i++;
+
         }
     }
 }
-    public static void main(String[] args){
-        fizzBUzz(100);
-    }
-    }
-
