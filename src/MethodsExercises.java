06abefe94a4736a8a141131306b1cc23c0e7f440
Allison Stull <allisonstull3@gmail.com>
2019-07-29 15:23:29 -0500

    still working

---
+++ src/MethodsExercises.java
@@ -0,0 +1,34 @@
+//METHODS exercise #1
+//Still need to complete part D
+
+public class MethodsExercises {
+
+    public static int Addition (int a ,int b) {
+        return(a + b);
+    }
+    public static int  Subtraction (int a, int b) {
+        return (a - b);
+    }
+    public static int Multiplication (int a, int b){
+        return (a * b);
+    }
+    public static int Division (int a, int b){
+        return (a / b);
+    }
+
+    public static void main (String[] args) {
+    System.out.println(Addition(5,6));
+        System.out.println(Subtraction(5,6));
+        System.out.println(Multiplication(5,6));
+        System.out.println(Division(5,6));
+
+
+
+
+    }
+
+}
+
+//#2
+
+
